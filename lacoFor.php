5e001107ab9b18be3a4df24120381fc6c6d7bd12
add-flav1s <aflavia17_@hotmail.com>
2021-07-06 15:43:58 -0300

    docs(for): aprendendo o laco de repeticao for.

---
+++ lacoFor.php
@@ -0,0 +1,9 @@
+<?php
+
+$tabuada = 9;
+
+for ($i = 1; $i <= 10; $i++) {
+    //enquanto i for igual ou menor que 10 laço FOR vai continuar rodando o codigo abaixo.
+    //i++ ta incrementando 1 na variavel i.
+    echo "$tabuada x $i = " . ($tabuada*$i) . PHP_EOL;
+}
