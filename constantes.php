90721486c87850d5dadbcd710f4de3ae0541ead0
add-flav1s <aflavia17_@hotmail.com>
2021-07-08 23:26:53 -0300

    docs(constantes): estudando constantes com arrays.

--- constantes.php
+++ constantes.php
@@ -2,4 +2,7 @@
 
 define ('NOME', 'Joao Pedro');
 define ('IDADE', '23 anos.');
-echo 'Me chamo ' . NOME . ' e tenho ' . IDADE;
+echo 'Me chamo ' . NOME . ' e tenho ' . IDADE . PHP_EOL;
+
+define ('ANIMES', ['naruto', 'one piece', 'demon slayer', 'jujutsu kaisen']);
+echo 'Um dos melhores animes do mundo é: ' . ANIMES[0];
