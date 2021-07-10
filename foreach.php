9aa198aefc11383e5bc5249cf40a519e0c0bc38e
add-flav1s <aflavia17_@hotmail.com>
2021-07-10 14:18:03 -0300

    docs(foreach): estudando foreach percorrendo um array.

---
+++ foreach.php
@@ -0,0 +1,8 @@
+<?php
+
+
+$personagensNaruto = ['naruto','sasuke','sakura','hinata','shikamaru','neji','kakashi','gaara','pain','temari'];
+
+foreach ($personagensNaruto as $valor){
+    echo $valor . PHP_EOL;
+}
