0a51ecd3aa7b3bee2243038a2afd6685b4f38ff0
add-flav1s <aflavia17_@hotmail.com>
2021-07-11 21:28:08 -0300

    docs(functions): estudando funcoes.

---
+++ calculadora.php
@@ -0,0 +1,26 @@
+<?php
+
+function calculadora($tabuada) {
+    for ($i = 1; $i <= 10; $i++) {
+        echo "$tabuada x $i = " . ($tabuada*$i) . '<br>';
+    }
+}
+calculadora(1);
+echo "<hr>";
+calculadora(2);
+echo "<hr>";
+calculadora(3);
+echo "<hr>";
+calculadora(4);
+echo "<hr>";
+calculadora(5);
+echo "<hr>";
+calculadora(6);
+echo "<hr>";
+calculadora(7);
+echo "<hr>";
+calculadora(8);
+echo "<hr>";
+calculadora(9);
+echo "<hr>";
+calculadora(10);
