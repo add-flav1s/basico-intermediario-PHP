ad63bf91d11c3cf3a123f22ec5c408b16374d66e
add-flav1s <aflavia17_@hotmail.com>
2021-07-12 22:23:06 -0300

    docs(POST): estudando o metodo post.

---
+++ formularios/index.php
@@ -0,0 +1,20 @@
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso de PHP</title>
+</head>
+
+<body>
+    <form method="post" action="dados.php">
+        Nome: <input type="text" name="nome">
+        <hr>
+        Email: <input type="email" name="email" id="">
+        <hr>
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+
+</html>
