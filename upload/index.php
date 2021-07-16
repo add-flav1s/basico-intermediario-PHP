c8f8bee5ae3f8f16a58599769fb60ca2848b02e8
add-flav1s <aflavia17_@hotmail.com>
2021-07-15 22:41:51 -0300

    docs(upload): fazendo melhorias no codigo.

--- upload/index.php
+++ upload/index.php
@@ -15,10 +15,20 @@ if(isset($_POST['enviar-formulario'])):
     $extensao = pathinfo ($_FILES['arquivo'] ['name'], PATHINFO_EXTENSION);
 
     if(in_array($extensao, $formatosPermitidos)):
-        echo 'Existe';
+       $pasta = "arquivos/";
+       $temporario = $_FILES['arquivo'] ['tmp_name'];
+       $novoNome = uniqid().".$extensao";
+
+        if(move_uploaded_file($temporario, $pasta.$novoNome)):
+            $mensagem = 'Upload feito com sucesso.';
+        else:
+            $mensagem = 'Não foi possivel fazer o upload.';
+        endif;
     else:
-        echo 'Nao existe';
+        $mensagem = 'Formato inválido.';
     endif;
+    
+    echo $mensagem;
 endif;
 ?>
     <form action="" method="post" enctype="multipart/form-data">
