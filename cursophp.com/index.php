1da9ea8992655fa56885f3f8dcab302838de6b52
add-flav1s <aflavia17_@hotmail.com>
2021-07-25 11:01:04 -0300

    docs(fix): form GET para um calculadora de multiplicacao.

---
+++ cursophp.com/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+.container {
+    width: 100vw;
+    height: 100vh;
+    background: #6C7A89;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center
+}
+
+.box {
+    width: 300px;
+    height: 300px;
+    background: #fff;
+    padding: 20px;
+    border-radius: 10px;
+}
+
+body {
+    margin: 0px;
+}
+</style>
+
+<body>
+    <div class="container">
+        <div class="box">
+
+            <h3>Multiplication Calculator</h3>
+            <form action="site.php" method="get">
+                Number 1: <input type="number" name="numberOne">
+                <br><br>
+                Number 2: <input type="number" name="numberTwo">
+                <br><br>
+                <input type="submit">
+            </form>
+            <br>
+            <hr>
+            <br>
+            <?php echo 'Answer: ' . $_GET["numberOne"] . ' x ' .  $_GET["numberTwo"] . ' = ' . $_GET["numberOne"] *  $_GET["numberTwo"]; ?>
+        </div>
+    </div>
+
+</body>
+
+</html>
